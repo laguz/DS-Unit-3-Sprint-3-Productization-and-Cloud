bea567d335602046c8072c26322ea57dcbea4c8f
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2021-04-20 21:52:28 -0400

    The the last update.

--- module2-consuming-data-from-an-api/twitoff/app.py
+++ module2-consuming-data-from-an-api/twitoff/app.py
@@ -2,7 +2,7 @@
 
 from os import getenv
 from flask import Flask, render_template
-from .models import DB, User
+from .models import DB, User, insert_example_users
 
 
 # creates application
@@ -20,6 +20,9 @@ def create_app():
     # decorator listens for specific endpoint visits
     @app.route('/') # http://127.0.0.1:5000/
     def root():
+        # we must create the database
+        #DB.drop_all()
+        #DB.create_all()
         # avoiding error since we are dropping all values - no duplicate users
         insert_example_users()
         # renders base.html template and passes down title and users
@@ -27,7 +30,7 @@ def create_app():
 
     @app.route('/update') # http://127.0.0.1:5000/allusers
     def update():
-        insert_example_user()
+        insert_example_users()
         return render_template("base.html", title="home", users=User.query.all())
 
     @app.route('/reset') # http://127.0.0.1:5000/allusers
@@ -36,4 +39,15 @@ def create_app():
         DB.drop_all()
         DB.create_all()
         return render_template("base.html", title="home")
+
+    @app.route('/iris')
+    def iris():
+        from sklearn.datasets import load_iris
+        from sklearn.linear_model import LogisticRegression
+        X, y = load_iris(return_X_y=True)
+        clf = LogisticRegression(random_state=0, solver='lbfgs',
+                              multi_class='multinomial').fit(X, y)
+
+        return str(clf.predict(X[:2, :]))
+
     return app
--- module2-consuming-data-from-an-api/twitoff/models.py
+++ module2-consuming-data-from-an-api/twitoff/models.py
@@ -1,7 +1,7 @@
 """SQLAlchemy models and utility functions for Twioff Application"""
 
 from flask_sqlalchemy import SQLAlchemy
-from .twitter import add_or_update_user
+#from .twitter import add_or_update_user
 
 
 DB = SQLAlchemy()
@@ -31,7 +31,14 @@ class Tweet(DB.Model):
     def __repr__(self):
         return "<Tweet: {}>".format(self.text)
 
-
-def insert_example_user():
+from .twitter import add_or_update_user
+def insert_example_users():
     """We will get an error if we run this twice witho droping & creating"""
     add_or_update_user("elonmusk")
+
+
+    """nick = User(id=1, name="nick")
+    elon = User(id=1, name="elonmusk")
+    DB.session.add(nick)
+    DB.session.add(elon)
+    DB.session.commit()"""
